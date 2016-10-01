66a1071a712c9b8f30252674bdbb422a7fd93496
Pablo <pabloridolfi@outlook.com>
2016-10-01 12:32:17 -0300

    adc_fir_dac improved for lpc4337

--- examples/adc_fir_dac/src/adc.c
+++ examples/adc_fir_dac/src/adc.c
@@ -22,23 +22,41 @@ void adcInit(void)
 	ADC_CLOCK_SETUP_T adc;
 
 	Chip_ADC_Init(LPC_ADC, &adc);
-	Chip_ADC_SetSampleRate(LPC_ADC, &adc, 22000);
+	Chip_ADC_SetSampleRate(LPC_ADC, &adc, 88000);
 
-	Chip_ADC_EnableChannel(LPC_ADC, ADC_CH0, ENABLE);
-	Chip_ADC_Int_SetChannelCmd(LPC_ADC, ADC_CH0, ENABLE);
+	Chip_ADC_EnableChannel(LPC_ADC, ADC_CH1, ENABLE);
+	Chip_ADC_Int_SetChannelCmd(LPC_ADC, ADC_CH1, ENABLE);
 	Chip_ADC_SetBurstCmd(LPC_ADC, ENABLE);
 
 	NVIC_EnableIRQ(ADC_IRQn);
 }
 
+#ifdef lpc4337_m4
+void ADC0_IRQHandler(void)
+#else
 void ADC_IRQHandler(void)
+#endif
 {
 	static uint16_t data;
-	Chip_ADC_ReadValue(LPC_ADC, ADC_CH0, &data);
+	Chip_ADC_ReadValue(LPC_ADC, ADC_CH1, &data);
+
+#ifdef lpc4337_m4
+	static uint32_t cont;
+	cont++;
+	if (cont == 4) {
+		cont = 0;
+#if(USAR_FUNCIONES_ASSEMBLER)
+		asm_fir_q31_put(&filtro, data);
+#else
+		fir_q31_put(&filtro, data);
+#endif
+	}
+#else
 #if(USAR_FUNCIONES_ASSEMBLER)
 	asm_fir_q31_put(&filtro, data>>2);
 #else
 	fir_q31_put(&filtro, data>>2);
+#endif
 #endif
 	adcFlag=1;
 }
--- examples/adc_fir_dac/src/dac.c
+++ examples/adc_fir_dac/src/dac.c
@@ -12,6 +12,9 @@
 void dacInit(void)
 {
 	Chip_DAC_Init(LPC_DAC);
+#ifdef lpc4337_m4
+	Chip_DAC_ConfigDAConverterControl(LPC_DAC, DAC_DMA_ENA);
+#endif
 }
 
 void dacWrite(uint32_t v)
