4fae9c163d36e7599ed4e193a6218dfb4dc5a429
Pablo Ridolfi <pabloridolfi@gmail.com>
2017-09-30 12:32:14 -0300

    add tick functions
    Signed-off-by: Pablo Ridolfi <pabloridolfi@gmail.com>

--- modules/lpc4337_m4/ciaa/inc/ciaaTick.h
+++ modules/lpc4337_m4/ciaa/inc/ciaaTick.h
@@ -1,7 +1,6 @@
-/* Copyright 2016, Pablo Ridolfi
- * All rights reserved.
+/* Copyright 2015, Pablo Ridolfi
  *
- * This file is part of Workspace.
+ * This file is part of CIAA-BM.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
@@ -30,42 +29,54 @@
  * POSSIBILITY OF SUCH DAMAGE.
  *
  */
- 
-#ifndef CIAAIO_H_
-#define CIAAIO_H_
 
-#include "chip.h"
+#ifndef _CIAATICK_H_
+#define _CIAATICK_H_
 
-#define ciaaDigitalInputs() ((uint8_t)((Chip_GPIO_ReadValue(LPC_GPIO_PORT,3) & (0x0F<<11))>>7)|(Chip_GPIO_ReadValue(LPC_GPIO_PORT,2) & 0x0F))
+/** @brief UART management.
+ **
+ **/
 
-typedef struct
-{
-	int port;
-	int bit;
-}ciaaPin_t;
+/** \addtogroup ciaa-bm CIAA-BM
+ ** @{ */
 
-typedef enum
-{
-	TEC1,
-	TEC2,
-	TEC3,
-	TEC4
-}edu_ciaa_nxp_tec_e;
+/*
+ * Initials     Name
+ * ---------------------------
+ * PR           Pablo Ridolfi
+ */
 
-typedef enum
-{
-	LEDR,
-	LEDG,
-	LEDB,
-	LED1,
-	LED2,
-	LED3
-}edu_ciaa_nxp_led_e;
+/*
+ * modification history (new versions first)
+ * -----------------------------------------------------------
+ * 20150612 v0.0.1   PR first version
+ */
 
-void ciaaIOInit(void);
-uint32_t ciaaWriteOutput(uint32_t outputNumber, uint32_t value);
-uint32_t ciaaReadInput(uint32_t inputNumber);
-uint32_t ciaaReadOutput(uint32_t outputNumber);
-void ciaaToggleOutput(uint32_t outputNumber);
+/*==================[inclusions]=============================================*/
 
-#endif /* CIAAIO_H_ */
+/*==================[cplusplus]==============================================*/
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/*==================[macros]=================================================*/
+
+/*==================[typedef]================================================*/
+
+/*==================[external data declaration]==============================*/
+
+/*==================[external functions declaration]=========================*/
+
+void pausems(int t);
+int ciaaTickInit(void);
+
+/*==================[cplusplus]==============================================*/
+
+#ifdef __cplusplus
+}
+#endif
+
+/** @} doxygen end group definition */
+/*==================[end of file]============================================*/
+#endif /* _CIAATICK_H_ */
